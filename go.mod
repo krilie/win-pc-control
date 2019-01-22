16711be5d3c29be1edb75693e43230a5861c44f5
acorn <776334655@qq.com>
2019-01-22 21:32:17 +0800

    init

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/lico603/win-pc-control
 // go: no requirements found in vendor/vendor.json
 
 require (
+	github.com/braintree/manners v0.0.0-20160418043613-82a8879fc5fd
 	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
 	github.com/gin-gonic/gin v1.3.0
 	github.com/golang/protobuf v1.2.0 // indirect
--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/braintree/manners"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, "ok")
-	})
 
-	if err := router.Run(":80"); err != nil {
+	//静态网页的处理
+	router.Static("/home", "./home")
+	router.GET("/api/hello", func(c *gin.Context) {
+		c.String(200, "api hello")
+	})
+	if err := manners.ListenAndServe(":80", router); err != nil {
 		fmt.Println(err)
 		return
 	}
